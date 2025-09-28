e7f0bdae9ef20141293f9b08dafaa355823fb3e3
Tingxuan Zhang <tingxuan.zhang0628@gmail.com>
2025-09-28 17:51:52 +1000

    Initial Commit

---
+++ NSWWater/NSWWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSWWaterApp.swift
+//  NSWWater
+//
+//  Created by 黑白熊 on 28/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct NSWWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
