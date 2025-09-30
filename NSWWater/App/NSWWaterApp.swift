cc29a064773af4fa3591a59265cb27cd1e0d581b
Tingxuan Zhang <tingxuan.zhang0628@gmail.com>
2025-10-01 00:02:32 +1000

    fix(bug)

--- NSWWater/App/NSWWaterApp.swift
+++ NSWWater/App/NSWWaterApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct NSWWaterApp: App {
+    @StateObject private var vm = DamListViewModel()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DamListView()
+                .environmentObject(vm)
         }
     }
 }
